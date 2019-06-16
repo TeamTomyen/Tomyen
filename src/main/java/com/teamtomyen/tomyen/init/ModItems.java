43028dd655c45cb7286e4c2275d40f76a66dab0a
TeamTomyen <teamtomyen@gmail.com>
2019-06-16 20:16:54 +0200

    start with Items - non working

---
+++ src/main/java/com/teamtomyen/tomyen/init/ModItems.java
@@ -0,0 +1,14 @@
+package com.teamtomyen.tomyen.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+
+public class ModItems {
+	
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item Hydrogen_Generator = new Itembase("hydrogen_generator");
+
+}
